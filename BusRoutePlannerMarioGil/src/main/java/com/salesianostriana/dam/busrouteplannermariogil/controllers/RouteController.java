da76e5d06dfe4e76fab325354db45a3be6f8e5dc
Mario <gil.gumar25@triana.salesianos.edu>
2026-05-11 14:30:25 +0200

    feat(show) muestra lista de rutas de autobuses

--- BusRoutePlannerMarioGil/src/main/java/com/salesianostriana/dam/busrouteplannermariogil/controllers/RouteController.java
+++ BusRoutePlannerMarioGil/src/main/java/com/salesianostriana/dam/busrouteplannermariogil/controllers/RouteController.java
@@ -3,17 +3,22 @@ package com.salesianostriana.dam.busrouteplannermariogil.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.busrouteplannermariogil.entity.Route;
 import com.salesianostriana.dam.busrouteplannermariogil.service.RouteService;
 
-import lombok.RequiredArgsConstructor;
-
-@Controller @RequiredArgsConstructor
+@Controller
 public class RouteController {
 	
 private final RouteService service;
-	
+
+public RouteController(RouteService servicio) {
+	this.service = servicio;
+}
+
+
 	@GetMapping("/Ruta")
 	public String showRutas(Model model) {
 		Route C2 = new Route(
@@ -25,7 +30,7 @@ private final RouteService service;
 				
 				);
 		
-		model.addAttribute("ruta",C2);
+		model.addAttribute("route",C2);
 		model.addAttribute("mensajeBienvenida","Muy buenas, te presentamos la mejor página web de rutas de autobuses del mundo, próximamente la Junta de Andalucía comprará esta página jeje");
 		
 		return "rutas";
@@ -35,15 +40,36 @@ private final RouteService service;
 	public String showLista(Model model) {
 		
 		model.addAttribute("routesList",service.getLista());
+		
 		return "listaRutas";
 	
 	}
+	
+	@GetMapping("/newRuta")
+	public String muestraFormulario(Model model) {
+	model.addAttribute("route", new Route());
+	return "registrousuario";
+	}
+	
+	@PostMapping("/newRuta/submit")
+	public String processForm(@ModelAttribute("route")Route ruta) {
+		service.addRuta(ruta);
+		
+		return "redirect:/";
+		
+		
+	}
+	
+	
 	
 	
 	@GetMapping("/Registro")
 	public String register(Model model) {
 		return "registrousuario";
 	}
+
+	
+	
 		
 
 }
--- BusRoutePlannerMarioGil/src/main/java/com/salesianostriana/dam/busrouteplannermariogil/service/RouteService.java
+++ BusRoutePlannerMarioGil/src/main/java/com/salesianostriana/dam/busrouteplannermariogil/service/RouteService.java
@@ -1,11 +1,14 @@
 package com.salesianostriana.dam.busrouteplannermariogil.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.busrouteplannermariogil.entity.Route;
+
+
 
 @Service
 public class RouteService {
@@ -14,7 +17,19 @@ public class RouteService {
 				new Route(152, "Palomares del Río","República Argentina",2),
 				new Route(153, "Palomares del Río","Blas Infante",3)
 				);
-		
+
 	}
+private List <Route> listaRutas = new ArrayList <Route>();
+	
+	public void addRuta (Route r) {
+		listaRutas.add(r);
+	
+	}
+	
+	public List<Route> getlistaRutas (){
+		return listaRutas; 
+	}
+	
+	
 
 }
