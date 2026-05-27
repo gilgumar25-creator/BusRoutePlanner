5fe8ab3adc2dc8617e27443a081bbe16510fbdfe
Mario <gil.gumar25@triana.salesianos.edu>
2026-05-27 14:13:09 +0200

    feat(delete): Implementada la función de eliminar planificación de ruta

--- BusRoutePlannerMarioGil/src/main/java/com/salesianostriana/dam/busrouteplannermariogil/entity/PlanificacionRuta.java
+++ BusRoutePlannerMarioGil/src/main/java/com/salesianostriana/dam/busrouteplannermariogil/entity/PlanificacionRuta.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -24,7 +25,7 @@ import lombok.NoArgsConstructor;
 public class PlanificacionRuta {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
     
     @DateTimeFormat(pattern = "yyyy-MM-dd")
